51e3883b59f3919cfdfb79f47113dbf3677a093a
Abdul Samad <abdulsamamd9004607@gmail.com>
2023-11-22 23:07:57 +0500

    contact page editing

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,9 @@ function sendMail() {
     var params = {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
+      phone: document.getElementById("phone").value,
+      service: document.getElementById("service").value,
+      message: document.getElementById("message").value
     };
   
     const serviceID = "service_s6pn1g6";
@@ -12,6 +14,8 @@ function sendMail() {
       .then(res=>{
           document.getElementById("name").value = "";
           document.getElementById("email").value = "";
+          document.getElementById("phone").value = "";
+          document.getElementById("service").value = "";
           document.getElementById("message").value = "";
           console.log(res);
           alert("Your message sent successfully!!")
@@ -23,3 +27,35 @@ function sendMail() {
       })
   
   }
+
+
+
+//   function sendMail() {
+//     var params = {
+//       name: document.getElementById("name").value,
+//       email: document.getElementById("email").value,
+//       phone: document.getElementById("phone").value,
+//       service: document.getElementById("service").value,
+//       message: document.getElementById("message").value
+//     };
+  
+//     const serviceID = "service_s6pn1g6";
+//     const templateID = "template_xcvumvh";
+  
+//       emailjs.send(serviceID, templateID, params)
+//       .then(res=>{
+//           document.getElementById("name").value = "";
+//           document.getElementById("email").value = "";
+//           document.getElementById("phone").value,
+//           document.getElementById("service").value,
+//           document.getElementById("message").value = "";
+//           console.log(res);
+//           alert("Your message sent successfully!!")
+  
+//       })
+//       .catch(err=>{
+//         console.log(err)
+//         alert("Your message not sent!!")
+//       })
+  
+//   }
