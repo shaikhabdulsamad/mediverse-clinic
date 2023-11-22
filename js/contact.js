a485a41aea6c346e6fed74e062d7546f348a226a
Abdul Samad <abdulsamamd9004607@gmail.com>
2023-11-23 02:09:21 +0500

    style editing

--- js/contact.js
+++ js/contact.js
@@ -1,61 +1,53 @@
+var getName = document.getElementById("name");
+var getEmail = document.getElementById("email");
+var getPhone = document.getElementById("phone");
+var getService = document.getElementById("service");
+var getMessage = document.getElementById("message");
+
 function sendMail() {
+  if (getName.value !== "" && getEmail.value !== "" && getPhone.value !== "" && getService.value !== "" && getMessage.value !== "") {
+
     var params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      phone: document.getElementById("phone").value,
-      service: document.getElementById("service").value,
-      message: document.getElementById("message").value
+      name: getName.value,
+      email: getEmail.value,
+      phone: getPhone.value,
+      service: getService.value,
+      message: getMessage.value
     };
-  
+
     const serviceID = "service_s6pn1g6";
     const templateID = "template_xcvumvh";
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("phone").value = "";
-          document.getElementById("service").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
+
+    emailjs.send(serviceID, templateID, params)
+      .then(res => {
+        getName.value = "";
+        getEmail.value = "";
+        getPhone.value = "";
+        getService.value = "";
+        getMessage.value = "";
+        console.log(res);
+        // alert("Your message sent successfully!!")
+        Swal.fire({
+          title: "Done!",
+          text: "Your message sent successfully!",
+          icon: "success"
+        });
+
       })
-      .catch(err=>{
+      .catch(err => {
         console.log(err)
         alert("Your message not sent!!")
+    
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong, your message not sent!"
+        
+        });
       })
-  
+
   }
 
 
+}
 
-//   function sendMail() {
-//     var params = {
-//       name: document.getElementById("name").value,
-//       email: document.getElementById("email").value,
-//       phone: document.getElementById("phone").value,
-//       service: document.getElementById("service").value,
-//       message: document.getElementById("message").value
-//     };
-  
-//     const serviceID = "service_s6pn1g6";
-//     const templateID = "template_xcvumvh";
-  
-//       emailjs.send(serviceID, templateID, params)
-//       .then(res=>{
-//           document.getElementById("name").value = "";
-//           document.getElementById("email").value = "";
-//           document.getElementById("phone").value,
-//           document.getElementById("service").value,
-//           document.getElementById("message").value = "";
-//           console.log(res);
-//           alert("Your message sent successfully!!")
-  
-//       })
-//       .catch(err=>{
-//         console.log(err)
-//         alert("Your message not sent!!")
-//       })
-  
-//   }
